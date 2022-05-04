945622000050f3a0fa71660fffdd7b4d21b296ce
Morgan Buntin <mmbuntin@gmail.com>
2022-05-04 10:15:47 -0400

    added two more tests

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -7,6 +7,22 @@ def test_get_all_books_with_no_records(client):
     assert response.status_code == 200
     assert response_body == []
 
+def test_get_all_books_with_records(client, two_saved_books):
+    # Act
+    response = client.get("/books")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == [{
+        "id": 1,
+        "title": "Ocean Book",
+        "description": "watr 4evr"
+    },
+    {   "id": 2,
+        "title": "Mountain Book",
+        "description": "i luv 2 climb rocks"}]
+
 def test_get_one_book(client, two_saved_books):
     # Act
     response = client.get("/books/1")
@@ -19,6 +35,15 @@ def test_get_one_book(client, two_saved_books):
         "title": "Ocean Book",
         "description": "watr 4evr"
     }
+
+def test_get_one_book_not_found(client):
+    # Act
+    response = client.get("/books/1")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 404
+    assert response_body["message"] == "book 1 not found"
 
 def test_create_one_book(client):
     # Act
